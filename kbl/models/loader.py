1bf10abcd3f05ba961a63c8b628fbd89547b3849
Krisztian Eyssen <krisz@eyssen.hu>
2020-05-19 00:35:37 +0200

    project.task loader

--- kbl/models/loader.py
+++ kbl/models/loader.py
@@ -19,7 +19,52 @@ class KblLoader(models.TransientModel):
 
     def load_from_kbl(self):
         
-        pass
+        # Server variables
+        url = 'https://kbl-legal.odoo.com'
+        db = 'kbl-legal-prod-495708'
+        username = 'odoosupport@eyssen.hu'
+        password = 'omakusoa'
+        
+        # Server Check
+        common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url), context=ssl._create_unverified_context())
+        uid = common.authenticate(db, username, password, {})
+        models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url), context=ssl._create_unverified_context())
+        
+        # Constants
+        COMPANY_ID = 1
+        PROJECT_PROJECT = {
+            2: 3,
+            3: 2,
+        }
+        PROJECT_TASK_TYPE = {
+            4: 1,
+            5: 2,
+            6: 3,
+            7: 4,
+        }
+        
+        # project.task
+        for oldTask in models.execute_kw(db, uid, password, 'project.task', 'search_read', [[['id', '>', 0]]],
+            {'fields': ['id', 'name', 'project_id', 'stage_id', 'sequence']}):
+            Task = self.env['project.task'].search([('x_studio_old_id', '=', oldTask['id'])])
+            if Task:
+                if Task.name != oldTask['name']:
+                    Task.name = oldTask['name']
+                if Task.project_id != PROJECT_PROJECT[oldTask['project_id'][0]]:
+                    Task.project_id = PROJECT_PROJECT[oldTask['project_id'][0]]
+                if Task.stage_id != PROJECT_TASK_TYPE[oldTask['stage_id'][0]]:
+                    Task.stage_id = PROJECT_TASK_TYPE[oldTask['stage_id'][0]]
+                if Task.sequence != oldTask['sequence']:
+                    Task.sequence = oldTask['sequence']
+            else:
+                self.env['project.task'].create({
+                    'company_id': COMPANY_ID,
+                    'x_studio_old_id': oldTask['id'],
+                    'name': oldTask['name'],
+                    'project_id': PROJECT_PROJECT[oldTask['project_id'][0]],
+                    'stage_id': PROJECT_TASK_TYPE[oldTask['stage_id'][0]],
+                    'sequence': oldTask['sequence'],
+                })
 
 
     def load_from_kozbeszguru(self):
