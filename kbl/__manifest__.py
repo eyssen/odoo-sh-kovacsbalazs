e2bac4721053b006a690067d4a835a357de28c71
Krisztian Eyssen <krisz@eyssen.hu>
2020-05-18 22:53:15 +0200

    KBL modul

---
+++ kbl/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'KBL',
+    'version' : '1.0',
+    'summary': 'KBL Addon',
+    'sequence': 15,
+    'description': """
+KBL Addon
+    """,
+    'category': 'Other',
+    'website': 'https://www.eyssen.hu',
+    'depends' : [],
+    'data': [
+        'views/loader.xml',
+    ],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ kbl/models/loader.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
+
+from os import listdir
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
+
+
+
+class KblLoader(models.TransientModel):
+    
+    _name = 'kbl.loader'
+
+
+    def load_from_kbl(self):
+        
+        pass
+
+
+    def load_from_kozbeszguru(self):
+        
+        pass
+
+
+    def load_from_ams(self):
+        
+        pass
