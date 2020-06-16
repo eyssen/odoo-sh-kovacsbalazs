7905b90f1316ec52420ac63e28b911f6da555e0c
Krisztian Eyssen <krisz@eyssen.hu>
2020-06-16 20:37:37 +0200

    Update loader.py

--- kbl/models/loader.py
+++ kbl/models/loader.py
@@ -276,110 +276,110 @@ class KblLoader(models.TransientModel):
             22: 3,
         }
         
-#         # res.partner
-#         _logger.info("== START Guru Partner ==")
-#         for x in [1, 2]:
-#             if x == 1:
-#                 oldPartners = models.execute_kw(db, uid, password, 'res.partner', 'search_read', [[['is_company', '=', 1]]],
-#                     {'fields': ['id', 'parent_id', 'name', 'active', 'customer', 'supplier', 'employee', 'type', 'street', 'street2', 'city', 'zip', 'phone', 'mobile', 'email', 'website', 'comment', 'vat_hu', 'vat', 'reg_number', 'create_date', 'write_date']})
-#             else:
-#                 oldPartners = models.execute_kw(db, uid, password, 'res.partner', 'search_read', [[['is_company', '=', 0]]],
-#                     {'fields': ['id', 'parent_id', 'name', 'active', 'customer', 'supplier', 'employee', 'type', 'street', 'street2', 'city', 'zip', 'phone', 'mobile', 'email', 'website', 'comment', 'vat_hu', 'vat', 'reg_number', 'create_date', 'write_date']})
-#             for oldPartner in oldPartners:
-#                 #TODO: country, tags (category_id), költségvetési pozíció
-#                 if oldPartner['id'] == 1:
-#                     oldPartner['id'] = 14
-#                 if oldPartner['parent_id'] == 1:
-#                     oldPartner['parent_id'] = 14
-#                 Partner = self.env['res.partner'].search([('company_id', '=', COMPANY_ID), ('old_id', '=', oldPartner['id'])])
-#                 if oldPartner['parent_id']:
-#                     ParentId = self.env['res.partner'].search([('company_id', '=', COMPANY_ID), ('old_id', '=', oldPartner['parent_id'][0])], limit=1)
-#                 else:
-#                     ParentId = None
-#                 if Partner:
-#                     if x == 1:
-#                         if Partner.is_company != True:
-#                             Partner.is_company = True
-#                     else:
-#                         if Partner.is_company != False:
-#                             Partner.is_company = False
-#                     if ParentId:
-#                         if Partner.parent_id.id != ParentId.id:
-#                             Partner.parent_id = ParentId.id
-#                     if Partner.name != oldPartner['name']:
-#                         Partner.name = oldPartner['name']
-#                     if Partner.active != oldPartner['active']:
-#                         Partner.active = oldPartner['active']
-#                     if Partner.customer_rank != oldPartner['customer']:
-#                         Partner.customer_rank = oldPartner['customer']
-#                     if Partner.supplier_rank != oldPartner['supplier']:
-#                         Partner.supplier_rank = oldPartner['supplier']
-#                     if Partner.employee != oldPartner['employee']:
-#                         Partner.employee = oldPartner['employee']
-#                     if Partner.type != oldPartner['type']:
-#                         Partner.type = oldPartner['type']
-#                     if Partner.street != oldPartner['street']:
-#                         Partner.tstreetype = oldPartner['street']
-#                     if Partner.street != oldPartner['street']:
-#                         Partner.street = oldPartner['street']
-#                     if Partner.street2 != oldPartner['street2']:
-#                         Partner.street2 = oldPartner['street2']
-#                     if Partner.city != oldPartner['city']:
-#                         Partner.city = oldPartner['city']
-#                     if Partner.zip != oldPartner['zip']:
-#                         Partner.zip = oldPartner['zip']
-#                     if Partner.phone != oldPartner['phone']:
-#                         Partner.phone = oldPartner['phone']
-#                     if Partner.mobile != oldPartner['mobile']:
-#                         Partner.mobile = oldPartner['mobile']
-#                     if Partner.email != oldPartner['email']:
-#                         Partner.email = oldPartner['email']
-#                     if Partner.website != oldPartner['website']:
-#                         Partner.website = oldPartner['website']
-#                     if Partner.comment != oldPartner['comment']:
-#                         Partner.comment = oldPartner['comment']
-#                     if Partner.vat_hu != oldPartner['vat_hu']:
-#                         Partner.vat_hu = oldPartner['vat_hu']
-#                     if Partner.vat != oldPartner['vat']:
-#                         Partner.vat = oldPartner['vat']
-#                     if Partner.reg_number != oldPartner['reg_number']:
-#                         Partner.reg_number = oldPartner['reg_number']
-#                     if Partner.create_date != oldPartner['create_date']:
-#                         Partner.create_date = oldPartner['create_date']
-#                     if Partner.write_date != oldPartner['write_date']:
-#                         Partner.write_date = oldPartner['write_date']
-#                 else:
-#                     vals = {
-#                         'company_id': COMPANY_ID,
-#                         'old_id': oldPartner['id'],
-#                         'name': oldPartner['name'],
-#                         'active': oldPartner['active'],
-#                         'customer_rank': oldPartner['customer'],
-#                         'supplier_rank': oldPartner['supplier'],
-#                         'employee': oldPartner['employee'],
-#                         'type': oldPartner['type'],
-#                         'street': oldPartner['street'],
-#                         'street2': oldPartner['street2'],
-#                         'city': oldPartner['city'],
-#                         'zip': oldPartner['zip'],
-#                         'phone': oldPartner['phone'],
-#                         'mobile': oldPartner['mobile'],
-#                         'email': oldPartner['email'],
-#                         'website': oldPartner['website'],
-#                         'vat_hu': oldPartner['vat_hu'],
-#                         'vat': oldPartner['vat'],
-#                         'reg_number': oldPartner['reg_number'],
-#                         'create_date': oldPartner['create_date'],
-#                         'write_date': oldPartner['write_date'],
-#                     }
-#                     if x == 1:
-#                         vals['is_company'] = True
-#                     else:
-#                         vals['is_company'] = False
-#                     if ParentId:
-#                         vals['parent_id'] = ParentId.id
-#                     self.env['res.partner'].create(vals)
-#         _logger.info("== END Guru Partner ==")
+        # res.partner
+        _logger.info("== START Guru Partner ==")
+        for x in [1, 2]:
+            if x == 1:
+                oldPartners = models.execute_kw(db, uid, password, 'res.partner', 'search_read', [[['is_company', '=', 1]]],
+                    {'fields': ['id', 'parent_id', 'name', 'active', 'customer', 'supplier', 'employee', 'type', 'street', 'street2', 'city', 'zip', 'phone', 'mobile', 'email', 'website', 'comment', 'vat_hu', 'vat', 'reg_number', 'create_date', 'write_date']})
+            else:
+                oldPartners = models.execute_kw(db, uid, password, 'res.partner', 'search_read', [[['is_company', '=', 0]]],
+                    {'fields': ['id', 'parent_id', 'name', 'active', 'customer', 'supplier', 'employee', 'type', 'street', 'street2', 'city', 'zip', 'phone', 'mobile', 'email', 'website', 'comment', 'vat_hu', 'vat', 'reg_number', 'create_date', 'write_date']})
+            for oldPartner in oldPartners:
+                #TODO: country, tags (category_id), költségvetési pozíció
+                if oldPartner['id'] == 1:
+                    oldPartner['id'] = 14
+                if oldPartner['parent_id'] == 1:
+                    oldPartner['parent_id'] = 14
+                Partner = self.env['res.partner'].search([('company_id', '=', COMPANY_ID), ('old_id', '=', oldPartner['id'])])
+                if oldPartner['parent_id']:
+                    ParentId = self.env['res.partner'].search([('company_id', '=', COMPANY_ID), ('old_id', '=', oldPartner['parent_id'][0])], limit=1)
+                else:
+                    ParentId = None
+                if Partner:
+                    if x == 1:
+                        if Partner.is_company != True:
+                            Partner.is_company = True
+                    else:
+                        if Partner.is_company != False:
+                            Partner.is_company = False
+                    if ParentId:
+                        if Partner.parent_id.id != ParentId.id:
+                            Partner.parent_id = ParentId.id
+                    if Partner.name != oldPartner['name']:
+                        Partner.name = oldPartner['name']
+                    if Partner.active != oldPartner['active']:
+                        Partner.active = oldPartner['active']
+                    if Partner.customer_rank != oldPartner['customer']:
+                        Partner.customer_rank = oldPartner['customer']
+                    if Partner.supplier_rank != oldPartner['supplier']:
+                        Partner.supplier_rank = oldPartner['supplier']
+                    if Partner.employee != oldPartner['employee']:
+                        Partner.employee = oldPartner['employee']
+                    if Partner.type != oldPartner['type']:
+                        Partner.type = oldPartner['type']
+                    if Partner.street != oldPartner['street']:
+                        Partner.tstreetype = oldPartner['street']
+                    if Partner.street != oldPartner['street']:
+                        Partner.street = oldPartner['street']
+                    if Partner.street2 != oldPartner['street2']:
+                        Partner.street2 = oldPartner['street2']
+                    if Partner.city != oldPartner['city']:
+                        Partner.city = oldPartner['city']
+                    if Partner.zip != oldPartner['zip']:
+                        Partner.zip = oldPartner['zip']
+                    if Partner.phone != oldPartner['phone']:
+                        Partner.phone = oldPartner['phone']
+                    if Partner.mobile != oldPartner['mobile']:
+                        Partner.mobile = oldPartner['mobile']
+                    if Partner.email != oldPartner['email']:
+                        Partner.email = oldPartner['email']
+                    if Partner.website != oldPartner['website']:
+                        Partner.website = oldPartner['website']
+                    if Partner.comment != oldPartner['comment']:
+                        Partner.comment = oldPartner['comment']
+                    if Partner.vat_hu != oldPartner['vat_hu']:
+                        Partner.vat_hu = oldPartner['vat_hu']
+                    if Partner.vat != oldPartner['vat']:
+                        Partner.vat = oldPartner['vat']
+                    if Partner.reg_number != oldPartner['reg_number']:
+                        Partner.reg_number = oldPartner['reg_number']
+                    if Partner.create_date != oldPartner['create_date']:
+                        Partner.create_date = oldPartner['create_date']
+                    if Partner.write_date != oldPartner['write_date']:
+                        Partner.write_date = oldPartner['write_date']
+                else:
+                    vals = {
+                        'company_id': COMPANY_ID,
+                        'old_id': oldPartner['id'],
+                        'name': oldPartner['name'],
+                        'active': oldPartner['active'],
+                        'customer_rank': oldPartner['customer'],
+                        'supplier_rank': oldPartner['supplier'],
+                        'employee': oldPartner['employee'],
+                        'type': oldPartner['type'],
+                        'street': oldPartner['street'],
+                        'street2': oldPartner['street2'],
+                        'city': oldPartner['city'],
+                        'zip': oldPartner['zip'],
+                        'phone': oldPartner['phone'],
+                        'mobile': oldPartner['mobile'],
+                        'email': oldPartner['email'],
+                        'website': oldPartner['website'],
+                        'vat_hu': oldPartner['vat_hu'],
+                        'vat': oldPartner['vat'],
+                        'reg_number': oldPartner['reg_number'],
+                        'create_date': oldPartner['create_date'],
+                        'write_date': oldPartner['write_date'],
+                    }
+                    if x == 1:
+                        vals['is_company'] = True
+                    else:
+                        vals['is_company'] = False
+                    if ParentId:
+                        vals['parent_id'] = ParentId.id
+                    self.env['res.partner'].create(vals)
+        _logger.info("== END Guru Partner ==")
         
         # project.task
         _logger.info("== START Guru Task ==")
@@ -749,33 +749,33 @@ class KblLoader(models.TransientModel):
                 self.env['project.task'].create(vals)
         _logger.info("== END Guru Task ==")
 
-#         # project.task.progress
-#         _logger.info("== START Guru Tevékenységek ==")
-#         for oldProgress in models.execute_kw(db, uid, password, 'project.task.progress', 'search_read', [[['id', '>', 0]]],
-#             {'fields': ['id', 'name', 'task_id', 'date', 'create_date', 'write_date']}):
-#             Progress = self.env['project.task.progress'].search([('company_id', '=', COMPANY_ID), ('old_id', '=', oldProgress['id'])])
-#             if Progress:
-#                 if Progress.name != oldProgress['name']:
-#                     Progress.name = oldProgress['name']
-#                 if Progress.date != oldProgress['date']:
-#                     Progress.date = oldProgress['date']
-#                 if Progress.create_date != oldProgress['create_date']:
-#                     Progress.create_date = oldProgress['create_date']
-#                 if Progress.write_date != oldProgress['write_date']:
-#                     Progress.write_date = oldProgress['write_date']
-#             else:
-#                 vals = {
-#                     'company_id': COMPANY_ID,
-#                     'old_id': oldProgress['id'],
-#                     'name': oldProgress['name'],
-#                     'task_id': self.env['project.task'].search([('company_id', '=', COMPANY_ID), ('old_id', '=', oldProgress['task_id'][0])], limit=1).id,
-#                     'date': oldProgress['date'],
-#                     'create_date': oldProgress['create_date'],
-#                     'write_date': oldProgress['write_date'],
-#                 }
-#                 if vals['task_id']:
-#                     self.env['project.task.progress'].create(vals)
-#         _logger.info("== END Guru Tevékenységek ==")
+        # project.task.progress
+        _logger.info("== START Guru Tevékenységek ==")
+        for oldProgress in models.execute_kw(db, uid, password, 'project.task.progress', 'search_read', [[['id', '>', 0]]],
+            {'fields': ['id', 'name', 'task_id', 'date', 'create_date', 'write_date']}):
+            Progress = self.env['project.task.progress'].search([('company_id', '=', COMPANY_ID), ('old_id', '=', oldProgress['id'])])
+            if Progress:
+                if Progress.name != oldProgress['name']:
+                    Progress.name = oldProgress['name']
+                if Progress.date != oldProgress['date']:
+                    Progress.date = oldProgress['date']
+                if Progress.create_date != oldProgress['create_date']:
+                    Progress.create_date = oldProgress['create_date']
+                if Progress.write_date != oldProgress['write_date']:
+                    Progress.write_date = oldProgress['write_date']
+            else:
+                vals = {
+                    'company_id': COMPANY_ID,
+                    'old_id': oldProgress['id'],
+                    'name': oldProgress['name'],
+                    'task_id': self.env['project.task'].search([('company_id', '=', COMPANY_ID), ('old_id', '=', oldProgress['task_id'][0])], limit=1).id,
+                    'date': oldProgress['date'],
+                    'create_date': oldProgress['create_date'],
+                    'write_date': oldProgress['write_date'],
+                }
+                if vals['task_id']:
+                    self.env['project.task.progress'].create(vals)
+        _logger.info("== END Guru Tevékenységek ==")
 
 
 
